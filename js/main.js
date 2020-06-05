484b6d39dc16021bcf5bb1412b897d9394c73ddd
Richie <mari4orl@mail.ru>
2020-06-05 15:57:53 +0300

    Add renderPins function, name magic numbers

--- js/main.js
+++ js/main.js
@@ -1,9 +1,23 @@
 'use strict';
 var MAX_PRICE = 10000;
+var MAP_WIDTH = 1200;
+var MAP_Y_1 = 130;
+var MAP_Y_2 = 630;
+var PIN_WIDTH = 50;
+var PIN_HEIGHT = 70;
+var adsNumber = 8;
 
 var types = ['palace', 'flat', 'house', 'bungalo'];
 var times = ['12:00', '13:00', '14:00'];
-var featuresArray = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+var features = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+
+var map = document.querySelector('.map');
+map.classList.remove('map--faded');
+
+var pinList = document.querySelector('.map__pins');
+var pinTemplate = document.querySelector('#pin')
+.content
+.querySelector('.map__pin');
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -11,28 +25,29 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; // Максимум не включается, минимум включается
 }
 
-function findNearestAd(adNumber) {
-  var nearestAdArray = [];
+function findNearestAd(number) {
+  var nearestAds = [];
 
-  for (var i = 0; i < adNumber; i++) {
+  for (var i = 0; i < number; i++) {
     var newFeaturesArray = [];
     var photosArray = [];
-    for (var j = 0; j <= getRandomInt(1, featuresArray.length); j++) {
-      newFeaturesArray[j] = featuresArray[j];
+    for (var j = 0; j <= getRandomInt(1, features.length); j++) {
+      newFeaturesArray[j] = features[j];
     }
 
     for (var k = 0; k <= getRandomInt(1, 10); k++) {
       photosArray[k] = 'http://o0.github.io/assets/images/tokyo/hotel' + k + '.jpg';
     }
-    var avaNumber = 1 + i;
+    var locationX = getRandomInt(0, MAP_WIDTH + 1);
+    var locationY = getRandomInt(MAP_Y_1, MAP_Y_2 + 1);
     var ad = {
       author: {
-        avatar: 'img/avatars/user0' + avaNumber + '.png'
+        avatar: 'img/avatars/user0' + (i + 1) + '.png'
       },
       offer: {
         title: 'Название объявления',
-        address: getRandomInt(0, 1201) + ',' + getRandomInt(130, 631),
-        price: getRandomInt(1, MAX_PRICE),
+        address: locationX + ',' + locationY,
+        price: getRandomInt(1, MAX_PRICE + 1),
         type: types[getRandomInt(0, types.length)],
         rooms: getRandomInt(1, 6),
         guests: getRandomInt(1, 6),
@@ -40,42 +55,37 @@ function findNearestAd(adNumber) {
         checkout: times[getRandomInt(0, times.length)],
         features: newFeaturesArray,
         description: 'Описание объявления',
-        photos: photosArray,
+        photos: photosArray
       },
       location: {
-        x: getRandomInt(0, 1201),
-        y: getRandomInt(130, 631)
+        x: locationX,
+        y: locationY
       }
     };
-    nearestAdArray[i] = ad;
+    nearestAds[i] = ad;
   }
-  return nearestAdArray;
+  return nearestAds;
 }
 
-var map = document.querySelector('.map');
-map.classList.remove('map--faded');
+var nearestAds = findNearestAd(adsNumber);
 
-var pinList = document.querySelector('.map__pins');
-var pinTemplate = document.querySelector('#pin')
-  .content
-  .querySelector('.map__pin');
-
-var nearestAdArray = findNearestAd(8);
-var fragment = document.createDocumentFragment();
-
-function renderPins(ad) {
+function renderPin(ad) {
   var adElement = pinTemplate.cloneNode(true);
 
-  adElement.style.left = ad.location.x - 25 + 'px';
-  adElement.style.top = ad.location.y - 70 + 'px';
+  adElement.style.left = ad.location.x - PIN_WIDTH / 2 + 'px';
+  adElement.style.top = ad.location.y - PIN_HEIGHT + 'px';
   adElement.querySelector('img').src = ad.author.avatar;
   adElement.querySelector('img').alt = ad.offer.title;
 
   return adElement;
 }
 
-for (var i = 0; i < nearestAdArray.length; i++) {
-  fragment.appendChild(renderPins(nearestAdArray[i]));
+function renderPins(adsArray, destination) {
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < adsArray.length; i++) {
+    fragment.appendChild(renderPin(adsArray[i]));
+  }
+  destination.appendChild(fragment);
 }
 
-pinList.appendChild(fragment);
+renderPins(nearestAds, pinList);
